bc65afa6e2b6274f756f8ac46f7080233a88d980
haruk2028 <haruk2028@student.cis.edu.hk>
2026-01-04 10:18:28 +0800

    Done!

---
+++ src/Cube.java
@@ -0,0 +1,40 @@
+public class Cube {
+
+    private int side;
+
+    public Cube() {
+        this.side = 1;
+    }
+
+    public Cube(int side) {
+        setSide(side);
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        validateSide(side);
+        this.side = side;
+    }
+
+    private void validateSide(int side) {
+        if (side < 1) {
+            throw new IllegalArgumentException("A cube’s side length cannot be less than 1!");
+        }
+    }
+
+    public int calculateSurfaceArea() {
+        return 6 * side * side;
+    }
+
+    public int calculateVolume() {
+        return side * side * side;
+    }
+
+    @Override
+    public String toString() {
+        return "Cube{side=" + side + "}";
+    }
+}
--- src/Cube2.java
+++ src/Cube2.java
@@ -2,28 +2,97 @@ public class Cube2 {
     private Cube basicCube;
     private String color;
 
+
     public Cube2() {
-        basicCube = new Cube();
+        this.basicCube = new Cube(1);
         this.color = "black";
     }
 
     public Cube2(int side) {
-        this();
-        basicCube.setSide(side);
+        this.basicCube = new Cube(side);
+        this.color = "black";
     }
+
+    public Cube2(int side, String color) {
+        this.basicCube = new Cube(side);
+        this.color = color;
+    }
+
 
     public int getSide() {
         return basicCube.getSide();
     }
 
-    //the following methods have been started for you, but need to be completed
-    public int calculateVolume() { return 0;}
-    public int calculateSurfaceArea() { return 0;}
-    public Cube2 add(Cube2 otherCube) { return new Cube2(); }
-    public Cube2 minus(Cube2 otherCube) { return new Cube2(); }
-    public boolean equals(Cube2 otherCube) { return false; }
+    public void setSide(int side) {
+        basicCube.setSide(side);
+    }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public int calculateSurfaceArea() {
+        int s = getSide();
+        return 6 * s * s;
+    }
+
+    public int calculateVolume() {
+        int s = getSide();
+        return s * s * s;
+    }
+
+
+    public boolean equals(Cube2 other) {
+        if (other == null) {
+            return false;
+        }
+        return this.getSide() == other.getSide()
+                && this.color.equals(other.color);
+    }
+
+
+    @Override
     public String toString() {
-        return basicCube.toString();
+        return "Cube{side=" + getSide() + ", color=\"" + color + "\"}";
+    }
+
+
+    public Cube2 add(Cube2 other) {
+        int a = this.getSide();
+        int b = other.getSide();
+
+        int cSquared = a * a + b * b;
+        int c = (int) Math.round(Math.sqrt(cSquared));
+
+        if (c * c != cSquared) {
+            throw new IllegalArgumentException();
+        }
+
+        return new Cube2(c, this.color);
+    }
+
+
+    public Cube2 minus(Cube2 other) {
+        int a = this.getSide();
+        int b = other.getSide();
+
+        int cSquared = a * a - b * b;
+
+        if (cSquared <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int c = (int) Math.round(Math.sqrt(cSquared));
+
+        if (c * c != cSquared) {
+            throw new IllegalArgumentException();
+        }
+
+        return new Cube2(c, this.color);
     }
 }
